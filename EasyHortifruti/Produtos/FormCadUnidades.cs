f288457b34b729ab10e3ae32214d7de70b169e4a
marcantossud <167142340+marcantossud@users.noreply.github.com>
2024-04-16 16:21:41 -0300

    Add files via upload

--- EasyHortifruti/Produtos/FormCadUnidades.cs
+++ EasyHortifruti/Produtos/FormCadUnidades.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace EasyHortifruti
 {
     public partial class FormCadUnidade : FormBase
@@ -29,19 +30,10 @@ namespace EasyHortifruti
                 return -1;
             }
         }
-        private void CarregarGrid()
+
+        private void btSairUnidades_Click(object sender, EventArgs e)
         {
-            ConexaoBD conexaoBD = new ConexaoBD();
-
-            dgvCadUnidades.DataSource = conexaoBD.ConsultarTabela(NomeTabelaBD); // dataset
-            dgvCadUnidades.DataMember = "Table"; // nome da tabela
-
-            // Especifica qual coluna deve ser usada para ordenação
-            DataGridViewColumn colunaParaOrdenar = dgvCadUnidades.Columns["ID"];
-
-            // Ordena os dados na coluna especificada
-            dgvCadUnidades.Sort(colunaParaOrdenar, System.ComponentModel.ListSortDirection.Ascending);
-
+            this.Close();
         }
 
         private void FormCadUnidade_Load(object sender, EventArgs e)
@@ -49,18 +41,8 @@ namespace EasyHortifruti
             NomeTabelaBD = "UNIDADES";
             CarregarGrid();
         }
-        private void BtIncluirUnidade_Click(object sender, EventArgs e)
-        {
-            // Criar uma nova instância do FormSecundario
-            FormUnidadeAltInsert UnidadeAltInsert = new FormUnidadeAltInsert();
 
-            // Exibir o FormSecundario
-            UnidadeAltInsert.ShowDialog();
-            CarregarGrid();
-
-        }
-
-        private void BtEditarUnidade_Click(object sender, EventArgs e)
+        private void btEditarUnidade_Click(object sender, EventArgs e)
         {
             DataGridViewSelectedRowCollection ds = dgvCadUnidades.SelectedRows;
 
@@ -79,7 +61,33 @@ namespace EasyHortifruti
             }
         }
 
-        private void BtExcluirUnidades_Click(object sender, EventArgs e)
+        private void btIncluirUnidade_Click(object sender, EventArgs e)
+        {
+            // Criar uma nova instância do FormSecundario
+            FormUnidadeAltInsert UnidadeAltInsert = new FormUnidadeAltInsert();
+
+            // Exibir o FormSecundario
+            UnidadeAltInsert.ShowDialog();
+            CarregarGrid();
+
+        }
+
+        private void CarregarGrid()
+        {
+            ConexaoBD conexaoBD = new ConexaoBD();
+
+            dgvCadUnidades.DataSource = conexaoBD.ConsultarTabela(NomeTabelaBD); // dataset
+            dgvCadUnidades.DataMember = "Table"; // nome da tabela
+
+            // Especifica qual coluna deve ser usada para ordenação
+            DataGridViewColumn colunaParaOrdenar = dgvCadUnidades.Columns["ID"];
+
+            // Ordena os dados na coluna especificada
+            dgvCadUnidades.Sort(colunaParaOrdenar, System.ComponentModel.ListSortDirection.Ascending);
+
+        }
+
+        private void btExcluirUnidades_Click(object sender, EventArgs e)
         {
             if (IdSelecionado >= 0)
             {
@@ -94,11 +102,5 @@ namespace EasyHortifruti
             else
                 MessageBox.Show("Selecione um registro para excluir");
         }
-
-        private void BtSairUnidades_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }       
-
     }
 }

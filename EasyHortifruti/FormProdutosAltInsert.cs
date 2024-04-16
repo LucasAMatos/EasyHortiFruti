96c243c2f4a7ce6962532f9be8ab96af05e7c3ae
marcantossud <167142340+marcantossud@users.noreply.github.com>
2024-04-15 23:52:19 -0300

    Add files via upload

---
+++ EasyHortifruti/FormProdutosAltInsert.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyHortifruti
+{
+    public partial class FormProdutosAltInsert : FormBase
+    {
+        public FormProdutosAltInsert()
+        {
+            InitializeComponent();
+        }
+
+        private void BtCancelaProduto_Click(object sender, EventArgs e)
+        {
+            // Fecha o formulário atual
+            this.Close();
+        }
+
+        private void BtCadUnidade_Click_1(object sender, EventArgs e)
+        {
+            // Criar uma nova instância do FormSecundario
+            FormCadUnidade FormCadUnidade = new FormCadUnidade();
+
+            // Exibir o FormSecundario
+            FormCadUnidade.Show();
+        }
+
+        private void BtCadGrupo_Click(object sender, EventArgs e)
+        {
+            // Criar uma nova instância do FormSecundario
+            FormCadGrupos CadGrupos = new FormCadGrupos();
+
+            // Exibir o FormSecundario
+            CadGrupos.Show();
+        }
+
+        private void BtCadSubGrupo_Click(object sender, EventArgs e)
+        {
+            // Criar uma nova instância do FormSecundario
+            FormCadSubGrupos CadSubGrupos = new FormCadSubGrupos();
+
+            // Exibir o FormSecundario
+            CadSubGrupos.Show();
+        }
+    }
+}

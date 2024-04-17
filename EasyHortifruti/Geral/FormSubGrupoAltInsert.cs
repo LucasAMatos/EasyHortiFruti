c200c7a3ff6b36130cfb87c2e546d67bc233a707
marcantossud <167142340+marcantossud@users.noreply.github.com>
2024-04-17 19:11:17 -0300

    atualização geral
    atualizando todos os dados do programa

---
+++ EasyHortifruti/Geral/FormSubGrupoAltInsert.cs
@@ -0,0 +1,68 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace EasyHortifruti
+{
+    public partial class FormSubGrupoAltInsert : FormBase
+    {
+        public FormSubGrupoAltInsert()
+        {
+            InitializeComponent();
+        }
+        private void FormSubGrupoAltInsert_Load(object sender, EventArgs e)
+        {
+            NomeTabelaBD = "GRUPO";
+            CarregarGridGrupo();
+        }
+
+        private void CarregarGridGrupo()
+        {
+            CbGrupo.Items.Clear();
+            DataSet ds = new ConexaoBD().ConsultarTabela("GRUPO");
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                CbGrupo.Items.Add(dr["nome_grupo"].ToString());
+            }
+        }
+
+        private void BtGravarSubGrupo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string Descricao = TbDescSubGrupo.Text; // Obtém o texto do TextBox
+                string Grupo = CbGrupo.Text; // Obtém o texto do TextBox
+                string MargemLucro = TbMargemSubGrupo.Text; // Obtém o texto do TextBox
+
+                if (idsubgrupo > 0)
+                {
+                    new ConexaoBD().AlterarSubGrupo(idgrupo, Descricao, Grupo, MargemLucro);
+                }
+                else
+                {
+                    new ConexaoBD().InserirSubGrupo(Descricao, Grupo, MargemLucro);
+                }
+
+                MessageBox.Show("GRAVOU");
+                this.Close();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+
+        private void BtCancelSubGrupo_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

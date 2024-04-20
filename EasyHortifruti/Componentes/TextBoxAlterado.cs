76aead8457920cf0a06e45c2c2bcf4779c0be061
LMatos <lmatos@banksofttecnologia.com.br>
2024-04-20 14:55:16 -0300

    LMATOS - Criando TextBox Alterado
    criando propriedade Obrigatorio

---
+++ EasyHortifruti/Componentes/TextBoxAlterado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyHortifruti.Componentes
+{
+    public partial class TextBoxAlterado : TextBox
+    {
+        // Adicionando uma propriedade personalizada
+        public bool Obrigatorio { get; set; }
+
+        public TextBoxAlterado()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}

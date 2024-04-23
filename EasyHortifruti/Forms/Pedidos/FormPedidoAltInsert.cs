77a722150cf83c9ca095646df20469055432792f
marcantossud <167142340+marcantossud@users.noreply.github.com>
2024-04-23 09:25:03 -0300

    Add files via upload

--- EasyHortifruti/Forms/Pedidos/FormPedidoAltInsert.cs
+++ EasyHortifruti/Forms/Pedidos/FormPedidoAltInsert.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿using EasyHortifruti.DML;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace EasyHortifruti
 {
     public partial class FormPedidoAltInsert : FormBase
     {
+        #region propriedades
+        Dictionary<string, int> geral;
+
+        #endregion
+
         private Timer timer;
         private Color originalColor;
         private Color warningColor = Color.Red; // Cor de aviso (vermelho)
@@ -19,6 +27,8 @@ namespace EasyHortifruti
 
         public FormPedidoAltInsert()
         {
+            geral = new Dictionary<string, int>();
+
             InitializeComponent();
 
             // Armazenar a cor original do label
@@ -32,6 +42,12 @@ namespace EasyHortifruti
             // Iniciar o Timer
             timer.Start();
         }
+        public FormPedidoAltInsert(int pId)
+        {
+            Id = pId;
+            geral = new Dictionary<string, int>();
+        }
+
         private void Timer_Tick(object sender, EventArgs e)
         {
             // Alternar entre a cor original e a cor de aviso
@@ -48,9 +64,49 @@ namespace EasyHortifruti
             warningDisplayed = !warningDisplayed;
         }
 
+        private void CbTpDocumento_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtBuscaClientePedido_Click(object sender, EventArgs e)
+        {
+            FormGeralCad BuscaCliente = new FormGeralCad();
+            BuscaCliente.ShowDialog();
+        }
+        private void CarregarGridNomeCliente()
+        {
+            CbNomeCliente.Items.Clear();
+            geral.Clear();
+            DataSet ds = new ConexaoBD().ConsultarGerais();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                geral.Add(dr["razao_social"].ToString(), Convert.ToInt32(dr["id_recno"]));
+                CbNomeCliente.Items.Add(dr["razao_social"].ToString());
+            }
+            CbNomeCliente.SelectedText = string.Empty;
+        }
+
+        private void CarregarComboStatusPedido()
+        {
+            CbStatusPedido.Items.Clear();
+            DataSet ds = new ConexaoBD().ConsultarUnidades();
+            foreach (StatusPedido item in Enum.GetValues(typeof(Sexo)))
+            {
+                CbStatusPedido.Items.Add(item);
+            }
+            CbStatusPedido.SelectedText = string.Empty;
+        }
+
+        private void FormPedidoAltInsert_Load(object sender, EventArgs e)
+        {
+
+        }
+
         private void BtCancelarPedido_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
     }
 }

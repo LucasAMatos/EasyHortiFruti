8d0193e0d7439b0fa6b4e8bac957547eb00333c2
marcantossud <marcantos.sud@gmail.com>
2024-04-24 16:58:40 -0300

    carregar combos e valores
    carregar combo e valores de produtos para calculo na tela de pedido

--- EasyHortifruti/Forms/Pedidos/FormPedidoAltInsert.cs
+++ EasyHortifruti/Forms/Pedidos/FormPedidoAltInsert.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,8 @@ namespace EasyHortifruti
 
         Dictionary<string, int> produtos;
 
+        Dictionary<string, int> unidade;
+
         #endregion
 
         private Timer timer;
@@ -31,6 +34,7 @@ namespace EasyHortifruti
         {
             geral = new Dictionary<string, int>();
             produtos = new Dictionary<String, int>();
+            unidade = new Dictionary<String, int>();
 
             InitializeComponent();
 
@@ -67,11 +71,6 @@ namespace EasyHortifruti
             warningDisplayed = !warningDisplayed;
         }
 
-        private void CbTpDocumento_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void BtBuscaClientePedido_Click(object sender, EventArgs e)
         {
             FormGeralCad BuscaCliente = new FormGeralCad();
@@ -101,6 +100,7 @@ namespace EasyHortifruti
             CarregarComboStatusPedido();
             CarregarGridNomeCliente();
             CarregarComboProdutos();
+            CarregarComboUnidades();
         }
         
         private void CarregarComboProdutos()
@@ -116,10 +116,107 @@ namespace EasyHortifruti
             CbProdutos.SelectedText = string.Empty;
         }
 
+        private void CbProdutos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Verifique se algum item está selecionado no ComboBox
+            if (CbProdutos.SelectedItem != null)
+            {
+                DataSet ds = new ConexaoBD().ConsultarProdutos();
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    produtos.Add(dr["pcocompra_produto"].ToString(), Convert.ToInt32(dr["id_recno"]));
+                    TbVlCompra.Text = (dr["pcocompra_produto"].ToString());
+                    produtos.Add(dr["margem_produto"].ToString(), Convert.ToInt32(dr["id_recno"]));
+                    TbMargemLucro.Text = (dr["margem_produto"].ToString());
+                }
+            }
+        }
+        
+        private void CarregarComboUnidades()
+        {
+            CbUnidPedido.Items.Clear();
+            unidade.Clear();
+            DataSet ds = new ConexaoBD().ConsultarUnidades();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                unidade.Add(dr["abrev_unid"].ToString(), Convert.ToInt32(dr["id_recno"]));
+                CbUnidPedido.Items.Add(dr["abrev_unid"].ToString());
+            }
+            CbUnidPedido.SelectedText = string.Empty;
+        }
+
+        private void CalcularValorFinal()
+        {
+            // Verifica se os TextBoxes têm valores válidos
+            if (decimal.TryParse(TbQtdPedido.Text, out decimal quantidade) &&
+                decimal.TryParse(TbVlCompra.Text, out decimal valorCompra) &&
+                decimal.TryParse(TbMargemLucro.Text, out decimal margemLucro))
+            {
+                // Calcula o valor da margem de lucro
+                decimal valorFinal = valorCompra * (1 + (margemLucro / 100)) * quantidade;
+
+                // Exibir a margem de lucro no TextBox de resultado
+                TbTotProdPedido.Text = valorFinal.ToString();
+            }
+            else
+            {
+                // Se os TextBoxes não tiverem valores válidos, limpa o TextBox da margem de lucro
+                TbTotProdPedido.Text = string.Empty;
+            }
+        }
+
         private void BtCancelarPedido_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        private void TbQtdPedido_TextChanged(object sender, EventArgs e)
+        {
+            CalcularValorFinal();
+        }
+
+        private void TbVlCompra_TextChanged(object sender, EventArgs e)
+        {
+            CalcularValorFinal();
+        }
+
+        private void TbMargemLucro_TextChanged(object sender, EventArgs e)
+        {
+            CalcularValorFinal();
+        }
+
+        private void CbNomeCliente_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CbNomeCliente.SelectedItem != null)
+            {
+                DataSet ds = new ConexaoBD().ConsultarGerais();
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    geral.Add(dr["celular"].ToString(), Convert.ToInt32(dr["id_recno"]));
+                    TbCelular.Text = (dr["celular"].ToString());
+
+                }
+            }
+        }
+
+        private void CbTpDocumento_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CbTpDocumento.SelectedItem != null)
+            {
+                // Verifique qual opção está selecionada no ComboBox
+               String opcaoSelecionada = CbTpDocumento.SelectedItem.ToString();
+
+                if (opcaoSelecionada == "1")
+                {
+                    PanelPfPedido.Visible = true;
+                    PanelPJPedido.Visible = false;
+                }
+                 else
+                {
+                    PanelPfPedido.Visible = false;
+                    PanelPJPedido.Visible = true;
+                }
+            }
+        }
     }
 }

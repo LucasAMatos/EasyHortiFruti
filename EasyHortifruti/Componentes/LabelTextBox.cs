c63cf52148409272f4ba177b8eb8715fb3fec215
LMatos <lucasa.matos@gmail.com>
2024-04-27 21:55:31 -0300

    #15 novo componente LabelTextBox
    novo componente que contém o Caption do textbox

---
+++ EasyHortifruti/Componentes/LabelTextBox.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyHortifruti.Componentes
+{
+    public partial class LabelTextBox : UserControl
+    {
+        #region Propriedades
+        public bool Obrigatorio { get; set; }
+
+        public bool Criticar
+        {
+            get
+            {
+                return Obrigatorio && string.IsNullOrEmpty(txtBox.Text);
+            }
+        }
+
+        public TipoCampo Tipo { get; set; }
+
+        public string Value
+        {
+            get
+            {
+                switch (Tipo)
+                {
+                    case TipoCampo.CEP:
+                    case TipoCampo.CPF:
+                    case TipoCampo.CNPJ:
+                    case TipoCampo.TELEFONE:
+                        return Regex.Replace(txtBox.Text, @"[^\d]+", "");
+
+                    default:
+                        return txtBox.Text;
+                }
+            }
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return label.Text;
+            }
+            set
+            {
+                label.Text = value;
+            }
+
+        }
+
+        public Font FonteTexto
+        {
+            get {
+                return txtBox.Font;
+            }
+            set { 
+                txtBox.Font = value;
+            }
+        }
+
+        public Font FonteCaption
+        {
+            get
+            {
+                return label.Font;
+            }
+            set
+            {
+                label.Font = value;
+            }
+        }
+
+        public int SelectionStart {
+            get {
+                return txtBox.SelectionStart;
+            }
+            set {
+                txtBox.SelectionStart = value;
+            }
+        }
+
+        public int SelectionLength
+        {
+            get
+            {
+                return txtBox.SelectionLength;
+            }
+            set
+            {
+                txtBox.SelectionLength = value;
+            }
+        }
+
+        public override string Text
+        {
+            get {
+                return txtBox.Text;
+            }
+            set {
+                txtBox.Text = value;
+            }
+        }
+
+        #endregion
+
+
+        public LabelTextBox()
+        {
+            InitializeComponent();
+        }
+
+        #region Metodos
+
+        private void FormataCampoLeave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Text))
+                return;
+
+            switch (Tipo)
+            {
+                case TipoCampo.TELEFONE:
+                    Text = Value.Length <= 10 ? Regex.Replace(Value, @"(\d{2})(\d{0,4})(\d{0,4})", @"($1)$2-$3") : Regex.Replace(Value, @"(\d{2})(\d{0,5})(\d{0,4})", @"($1)$2-$3");
+                    break;
+
+                case TipoCampo.CEP:
+                    Text = Regex.Replace(Value.PadRight(8, '0'), @"(\d{5})(\d{0,3})", @"$1-$2");
+                    break;
+
+                case TipoCampo.CNPJ:
+                    Text = Regex.Replace(Value.PadLeft(14, '0'), @"(\d{2})(\d{3})(\d{3})(\d{4})(\d{0,2})", @"$1.$2.$3/$4-$5");
+                    break;
+
+                case TipoCampo.CPF:
+                    Text = Regex.Replace(Value.PadLeft(11, '0'), @"(\d{3})(\d{3})(\d{3})", @"$1.$2.$3-");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void FormataCampos(object sender, KeyPressEventArgs e)
+        {
+            switch (Tipo)
+            {
+                case TipoCampo.TELEFONE:
+                    // Permite apenas números e teclas de controle (como Backspace)
+                    if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                        e.Handled = true;
+
+                    if (char.IsDigit(e.KeyChar))
+                    {
+                        if (Value.Length < 11)
+                        {
+                            if (Value.Length >= 10)
+                                Text = Regex.Replace(Value, @"(^\d{0,2})(\d{0,5})(\d{0,4})", @"($1)$2-$3");
+                            else if (Value.Length >= 6)
+                                Text = Regex.Replace(Value, @"(^\d{0,2})(\d{0,4})(\d{0,4})", @"($1)$2-$3");
+                            else if (Value.Length >= 2)
+                                Text = Regex.Replace(Value, @"(^\d{0,2})", @"($1)");
+                            else
+                                Text = "(" + Value;
+
+                            this.SelectionStart = this.Text.Length;
+                        }
+                    }
+                    return;
+
+                case TipoCampo.CEP:
+                    // Permite apenas números e teclas de controle (como Backspace)
+                    if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                        e.Handled = true;
+
+                    if (char.IsDigit(e.KeyChar))
+                        Text = Value.Length >= 5 ? string.Format("{0}-{1}", Value.Substring(0, 5), Value.Substring(5, Value.Length - 5)) : Value;
+
+                    this.SelectionStart = this.Text.Length;
+                    return;
+
+                case TipoCampo.CPF:
+                    // Permite apenas números e teclas de controle (como Backspace)
+                    if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                        e.Handled = true;
+
+                    if (char.IsDigit(e.KeyChar))
+                    {
+                        if (Value.Length < 11)
+                        {
+                            Text += e.KeyChar;
+
+                            if (Value.Length >= 9)
+                                Text = Regex.Replace(Value, @"(\d{3})(\d{3})(\d{3})", @"$1.$2.$3-");
+                            else if (Value.Length >= 3)
+                                Text = Regex.Replace(Value, @"(\d{3})", @"$1.");
+                            else
+                                Text = Value;
+
+                            this.SelectionStart = this.Text.Length;
+                        }
+                        e.Handled = true;
+                    }
+                    return;
+
+                case TipoCampo.CNPJ:
+                    // Permite apenas números e teclas de controle (como Backspace)
+                    if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                        e.Handled = true;
+
+                    if (char.IsDigit(e.KeyChar))
+                    {
+                        if (Value.Length < 14)
+                        {
+                            Text += e.KeyChar;
+
+                            if (Value.Length >= 12)
+                                Text = Regex.Replace(Value, @"(\d{2})(\d{3})(\d{3})(\d{4})(\d{0,2})", @"$1.$2.$3/$4-$5");
+                            else if (Value.Length >= 8)
+                                Text = Regex.Replace(Value, @"(\d{2})(\d{3})(\d{3})(\d{0,4})", @"$1.$2.$3/$4");
+                            else if (Value.Length >= 5)
+                                Text = Regex.Replace(Value, @"(\d{2})(\d{3})(\d{0,3})", @"$1.$2.$3");
+                            else if (Value.Length >= 3)
+                                Text = Regex.Replace(Value, @"(\d{2})(\d{0,3})", @"$1.$2");
+
+                            this.SelectionStart = this.Text.Length;
+                        }
+                        e.Handled = true;
+                    }
+                    return;
+
+                case TipoCampo.MOEDA:
+                    if ((!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',') ||
+                       (Text.Contains(',') && e.KeyChar == ','))
+                    {
+                        e.Handled = true;
+                        return;
+                    }
+                    if (string.IsNullOrEmpty(Text) && e.KeyChar == ',')
+                    {
+                        e.Handled = true;
+                        Text = "0,";
+                        SelectionStart = Text.Length;
+                        return;
+                    }
+                    if (!char.IsControl(e.KeyChar) && Text.Contains(',') && Text.Split(',')[1].Length == 2 && SelectionLength == 0)
+                    {
+                        e.Handled = true;
+                        return;
+                    }
+                    if (e.KeyChar == 8 && SelectionLength > 0)
+                    {
+                        Text = Text.Remove(SelectionStart, SelectionLength);
+                        SelectionStart = Text.Length;
+                        SelectionLength = 0;
+                        e.Handled = true;
+                        return;
+                    }
+                    if (e.KeyChar == 8 && SelectionLength == 0)
+                    {
+                        if (Text.Length > 0)
+                        {
+                            Text = Text.Remove(SelectionStart - 1);
+                            SelectionStart = Text.Length;
+                            SelectionLength = 0;
+                            e.Handled = true;
+                            return;
+                        }
+                    }
+                    return;
+
+                default:
+                    return;
+            }
+        }
+
+        #endregion Metodos
+
+
+        #region Enum
+
+        public enum TipoCampo
+        {
+            TEXTO,
+            CPF,
+            CNPJ,
+            TELEFONE,
+            CEP,
+            MOEDA
+        }
+
+        #endregion Enum
+    }
+}
--- EasyHortifruti/Forms/Produtos/FormUnidadesAltInsert.Designer.cs
+++ EasyHortifruti/Forms/Produtos/FormUnidadesAltInsert.Designer.cs
@@ -34,12 +34,9 @@ namespace EasyHortifruti
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormUnidadesAltInsert));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.tbCadObsUni = new EasyHortifruti.Componentes.AltTextBox();
-            this.lbCadObsUnid = new System.Windows.Forms.Label();
-            this.tbCadDescricaoUni = new EasyHortifruti.Componentes.AltTextBox();
-            this.lbcadDescriUni = new System.Windows.Forms.Label();
-            this.tbCadAbreviUni = new EasyHortifruti.Componentes.AltTextBox();
-            this.lbCadAbrevUni = new System.Windows.Forms.Label();
+            this.tbCadObsUni = new EasyHortifruti.Componentes.LabelTextBox();
+            this.tbCadDescricaoUni = new EasyHortifruti.Componentes.LabelTextBox();
+            this.tbCadAbreviUni = new EasyHortifruti.Componentes.LabelTextBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.lbIDCadUnidade = new System.Windows.Forms.Label();
             this.BtGravarUnidade = new System.Windows.Forms.Button();
@@ -66,11 +63,8 @@ namespace EasyHortifruti
             // panel4
             // 
             this.panel4.Controls.Add(this.tbCadObsUni);
-            this.panel4.Controls.Add(this.lbCadObsUnid);
             this.panel4.Controls.Add(this.tbCadDescricaoUni);
-            this.panel4.Controls.Add(this.lbcadDescriUni);
             this.panel4.Controls.Add(this.tbCadAbreviUni);
-            this.panel4.Controls.Add(this.lbCadAbrevUni);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel4.Location = new System.Drawing.Point(0, 42);
             this.panel4.Name = "panel4";
@@ -79,64 +73,48 @@ namespace EasyHortifruti
             // 
             // tbCadObsUni
             // 
-            this.tbCadObsUni.Caption = null;
-            this.tbCadObsUni.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCadObsUni.Location = new System.Drawing.Point(31, 186);
-            this.tbCadObsUni.Multiline = true;
+            this.tbCadObsUni.Caption = "Observação da Unidade";
+            this.tbCadObsUni.FonteCaption = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.tbCadObsUni.FonteTexto = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCadObsUni.Location = new System.Drawing.Point(31, 178);
+            this.tbCadObsUni.Margin = new System.Windows.Forms.Padding(4);
             this.tbCadObsUni.Name = "tbCadObsUni";
             this.tbCadObsUni.Obrigatorio = false;
-            this.tbCadObsUni.Size = new System.Drawing.Size(408, 103);
+            this.tbCadObsUni.SelectionLength = 0;
+            this.tbCadObsUni.SelectionStart = 0;
+            this.tbCadObsUni.Size = new System.Drawing.Size(408, 112);
             this.tbCadObsUni.TabIndex = 5;
-            // 
-            // lbCadObsUnid
-            // 
-            this.lbCadObsUnid.AutoSize = true;
-            this.lbCadObsUnid.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbCadObsUnid.Location = new System.Drawing.Point(28, 168);
-            this.lbCadObsUnid.Name = "lbCadObsUnid";
-            this.lbCadObsUnid.Size = new System.Drawing.Size(160, 15);
-            this.lbCadObsUnid.TabIndex = 4;
-            this.lbCadObsUnid.Text = "Observação da Unidade";
+            this.tbCadObsUni.Tipo = EasyHortifruti.Componentes.LabelTextBox.TipoCampo.TEXTO;
             // 
             // tbCadDescricaoUni
             // 
-            this.tbCadDescricaoUni.Caption = null;
-            this.tbCadDescricaoUni.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCadDescricaoUni.Location = new System.Drawing.Point(31, 121);
+            this.tbCadDescricaoUni.Caption = "Descrição";
+            this.tbCadDescricaoUni.FonteCaption = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.tbCadDescricaoUni.FonteTexto = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCadDescricaoUni.Location = new System.Drawing.Point(31, 109);
+            this.tbCadDescricaoUni.Margin = new System.Windows.Forms.Padding(4);
             this.tbCadDescricaoUni.Name = "tbCadDescricaoUni";
             this.tbCadDescricaoUni.Obrigatorio = false;
-            this.tbCadDescricaoUni.Size = new System.Drawing.Size(408, 24);
+            this.tbCadDescricaoUni.SelectionLength = 0;
+            this.tbCadDescricaoUni.SelectionStart = 0;
+            this.tbCadDescricaoUni.Size = new System.Drawing.Size(408, 49);
             this.tbCadDescricaoUni.TabIndex = 3;
-            // 
-            // lbcadDescriUni
-            // 
-            this.lbcadDescriUni.AutoSize = true;
-            this.lbcadDescriUni.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbcadDescriUni.Location = new System.Drawing.Point(28, 103);
-            this.lbcadDescriUni.Name = "lbcadDescriUni";
-            this.lbcadDescriUni.Size = new System.Drawing.Size(71, 15);
-            this.lbcadDescriUni.TabIndex = 2;
-            this.lbcadDescriUni.Text = "Descrição";
+            this.tbCadDescricaoUni.Tipo = EasyHortifruti.Componentes.LabelTextBox.TipoCampo.TEXTO;
             // 
             // tbCadAbreviUni
             // 
-            this.tbCadAbreviUni.Caption = null;
-            this.tbCadAbreviUni.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCadAbreviUni.Location = new System.Drawing.Point(31, 62);
+            this.tbCadAbreviUni.Caption = "Abreviatura";
+            this.tbCadAbreviUni.FonteCaption = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.tbCadAbreviUni.FonteTexto = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCadAbreviUni.Location = new System.Drawing.Point(31, 39);
+            this.tbCadAbreviUni.Margin = new System.Windows.Forms.Padding(4);
             this.tbCadAbreviUni.Name = "tbCadAbreviUni";
             this.tbCadAbreviUni.Obrigatorio = false;
-            this.tbCadAbreviUni.Size = new System.Drawing.Size(81, 24);
+            this.tbCadAbreviUni.SelectionLength = 0;
+            this.tbCadAbreviUni.SelectionStart = 0;
+            this.tbCadAbreviUni.Size = new System.Drawing.Size(408, 48);
             this.tbCadAbreviUni.TabIndex = 1;
-            // 
-            // lbCadAbrevUni
-            // 
-            this.lbCadAbrevUni.AutoSize = true;
-            this.lbCadAbrevUni.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbCadAbrevUni.Location = new System.Drawing.Point(30, 44);
-            this.lbCadAbrevUni.Name = "lbCadAbrevUni";
-            this.lbCadAbrevUni.Size = new System.Drawing.Size(79, 15);
-            this.lbCadAbrevUni.TabIndex = 0;
-            this.lbCadAbrevUni.Text = "Abreviatura";
+            this.tbCadAbreviUni.Tipo = EasyHortifruti.Componentes.LabelTextBox.TipoCampo.TEXTO;
             // 
             // panel3
             // 
@@ -213,19 +191,18 @@ namespace EasyHortifruti
             // 
             this.unidadesBindingSource.DataMember = "unidades";
             // 
-            // FormUnidadeAltInsert
+            // FormUnidadesAltInsert
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(476, 450);
             this.Controls.Add(this.panel1);
-            this.Name = "FormUnidadeAltInsert";
+            this.Name = "FormUnidadesAltInsert";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "EasyHortifruti - Inserir Unidade";
             this.Load += new System.EventHandler(this.FormUnidadeAltInsert_Load);
             this.panel1.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.unidadesBindingSource)).EndInit();
@@ -238,13 +215,10 @@ namespace EasyHortifruti
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Label lbcadDescriUni;
-        private AltTextBox tbCadAbreviUni;
-        private System.Windows.Forms.Label lbCadAbrevUni;
+        private LabelTextBox tbCadAbreviUni;
         private System.Windows.Forms.Panel panel3;
-        private AltTextBox tbCadDescricaoUni;
-        private AltTextBox tbCadObsUni;
-        private System.Windows.Forms.Label lbCadObsUnid;
+        private LabelTextBox tbCadDescricaoUni;
+        private LabelTextBox tbCadObsUni;
         private System.Windows.Forms.Button BtGravarUnidade;
         private System.Windows.Forms.Button BtCancelCadUnidade;
         private System.Windows.Forms.Label lbIDCadUnidade;
